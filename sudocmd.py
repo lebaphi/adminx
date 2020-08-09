c79ef7c80eb1336b64eeb776e9b0c4e85da48b79
philb89 <everlasting2605@gmail.com>
2020-08-10 01:04:16 +0700

    test

--- sudocmd.py
+++ sudocmd.py
@@ -2,8 +2,7 @@ from utils import runCmd
 
 
 def getListSudoCMD():
-    runCmd(
-        "HISTFILE=~/.bash_history set -o history HISTTIMEFORMAT='Date %d %b %T ' history|awk '{$1="
-        "}1''{$5='SUDO COMMAND='}5'"
-    )
+    runCmd("HISTFILE=~/.bash_history")
+    runCmd("set -o history")
+    runCmd("HISTTIMEFORMAT=\"Date %d %b %T \" history|awk '{$1=\"\"}1''{$5=\"SUDO COMMAND=\"}5'")
 
--- utils.py
+++ utils.py
@@ -2,10 +2,5 @@ import os
 
 
 def runCmd(cmd):
-    # args = cmd.split(" ")
-    # result = subprocess.call(
-    #     args, stdout=subprocess.PIPE, shell=True, universal_newlines=True, check=True
-    # )
-    # print(result.stdout.decode("utf-8"))
     os.system(cmd)
 
